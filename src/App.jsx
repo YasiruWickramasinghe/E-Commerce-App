ff1d374cfca8fb35d00987cbd8e8a30e07b9b149
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-06-03 18:47:59 +0530

    E commerce app initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome e-commerce app...!</header>
+    </div>
+  );
+}
+
+export default App;
